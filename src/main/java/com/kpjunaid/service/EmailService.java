69fc55222a10950343ce7882cc7967d836698b1d
freddy.ojeda <freddyeojeda@gmail.com>
2022-09-08 09:59:50 -0500

    configuracion paquetes

--- src/main/java/com/kpjunaid/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.kpjunaid.service;
-
-public interface EmailService {
-    void send(String to, String subject, String content);
-    String buildEmailVerifyMail(String token);
-    String buildResetPasswordMail(String token);
-
-    void sendMail(String from,String to,String subject,String body);
-}
